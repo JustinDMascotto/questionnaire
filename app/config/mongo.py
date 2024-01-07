d76ea8579c2494c89b2685a1214b4946a991f494
Justin Mascotto <mascottoj@gmail.com>
2024-01-07 15:13:17 -0500

    add environments

--- app/config/mongo.py
+++ app/config/mongo.py
@@ -1,10 +1,14 @@
 from pymongo import MongoClient
+from flask import current_app
+from dotenv import load_dotenv
+import os
 
 class MongoSingleton:
     _instance = None
 
     @classmethod
     def get_instance(cls):
+        load_dotenv('.env/' + current_app.config["ENV"] )
         if not cls._instance:
-            cls._instance = MongoClient("mongodb://admin:admin@localhost:27017")
+            cls._instance = MongoClient(os.environ.get('MONGO_URL'))
         return cls._instance
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import create_app
 
-app = create_app()
+env = 'dev'
+app = create_app(env)
 
 if __name__ == '__main__':
     app.run(debug=True)
