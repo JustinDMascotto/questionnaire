5a00821ba878815f4ea2d5f0cf1361c63793047e
Justin Mascotto <mascottoj@gmail.com>
2024-01-18 18:32:40 -0500

    add put and get endpoints for questionnaires

--- app/routes/questionnaire_routes.py
+++ app/routes/questionnaire_routes.py
@@ -15,7 +15,6 @@ def create_questionnaire():
     data = request.get_json()
     try:
         questionnaire = Questionnaire(**data)
-        print(questionnaire)
     except TypeError as e:
         return jsonify({'message':f"invalid data {e}",}), 400
     questionnaire.createdAt = datetime.utcnow()
@@ -26,12 +25,44 @@ def create_questionnaire():
     }
     questionnaire._id = str(id)
     questionnaire_id = MongoSingleton.get_instance().flask_db.questionnaires.insert_one(asdict(questionnaire))
-    return jsonify({"_id": questionnaire_id.inserted_id}), 201
+    return jsonify({'versionedId': questionnaire.versionedId}), 201
 
+@questionnaire_blueprint.route( base_route + '/<id>', methods=['PUT'])
+@require_auth
+def update_questionnaire(id):
+    data = request.get_json()
+    try:
+        questionnaire = Questionnaire(**data)
+    except TypeError as e:
+        return jsonify({'message':f"invalid data {e}",}), 400
+    questionnaire.createdAt = datetime.utcnow()
+    latest_questionnaire = MongoSingleton.get_instance().flask_db.questionnaires.find({'versionedId._id':id}).sort({'versionedId.version':-1}).limit(1).next()
+    pk_id = uuid4()
+    questionnaire.versionedId = {
+        "_id":str(id),
+        "version": str(int(latest_questionnaire['versionedId']['version']) + 1)
+    }
+    questionnaire._id = str(pk_id)
+    questionnaire_id = MongoSingleton.get_instance().flask_db.questionnaires.insert_one(asdict(questionnaire))
+    return jsonify({'versionedId': questionnaire.versionedId}), 200
 
 @questionnaire_blueprint.route( base_route + '/<id>', methods=['GET'])
-def get_questionnaire(id):
-    questionnaire = MongoSingleton.get_instance().flask_db.questionnaires.find_one({"_id":id})
+def get_questionnaire_versions(id):
+    questionnaires = list(MongoSingleton.get_instance().flask_db.questionnaires.find({"versionedId._id":id}))
+    if questionnaires:
+        return jsonify(questionnaires), 200
+    return jsonify({'error': 'Questionnaire not found'}), 404
+
+@questionnaire_blueprint.route( base_route + '/<id>/<version>', methods=['GET'])
+def get_questionnaire(id,version):
+    if version == 'latest':
+        query = {'versionedId._id':id}
+    else:
+        query = {"versionedId":{
+        "_id":id,
+        "version":version
+    }}
+    questionnaire = MongoSingleton.get_instance().flask_db.questionnaires.find(query).sort({'versionedId.version':-1}).limit(1).next()
     if questionnaire:
         return jsonify(questionnaire), 200
     return jsonify({'error': 'Questionnaire not found'}), 404
